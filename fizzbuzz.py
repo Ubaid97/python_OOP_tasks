ced5e07adeb4172b469d1ff290ad9bc40c9afdb4
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-05 15:12:11 +0000

    pseudo-coding

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,23 @@
 class FizzBuzz:
 
+    # for loop ranges over numbers from 1 to 100
     for i in range(1, 101):
+
+        # if the number is a multiple of both 3 and 5, it prints out "FizzBuzz"
         if (i % 3 == 0) and (i % 5 == 0):
             print("FizzBuzz")
+
+        # if a multiple of 3 only, prints out Fizz
         elif i % 3 == 0:
             print("Fizz")
+
+        # if a multiple of 5 only, prints out Buzz
         elif i % 5 == 0:
             print("Buzz")
+
+        # if a multiple of neither, prints out the number
         else:
             print(i)
+
+        # increments i by 1 in each loop until the end of the specified range
         i += 1
--- fizzbuzz2.py
+++ fizzbuzz2.py
@@ -1,15 +1,27 @@
 class FizzBuzz2:
 
+        # users are asked to input the two numbers for which they'd like fizz and buzz substituted
         num1 = int(input("Please enter your fizz number: "))
         num2 = int(input("Please enter your buzz number: "))
 
+    # for loop ranges over numbers from 1 to 100
         for i in range(1, 101):
+
+            # if the number is a multiple of both numbers passed by the user, it prints out "FizzBuzz"
             if (i % num1 == 0) and (i % num2 == 0):
                 print("FizzBuzz")
+
+            # if a multiple of the first number only, prints out Fizz
             elif i % num1 == 0:
                 print("Fizz")
+
+            # if a multiple of the second number only, prints out Buzz
             elif i % num2 == 0:
                 print("Buzz")
+
+            # if a multiple of neither, prints out the number
             else:
                 print(i)
+
+            # increments i by 1 in each loop until the end of the specified range
             i += 1
